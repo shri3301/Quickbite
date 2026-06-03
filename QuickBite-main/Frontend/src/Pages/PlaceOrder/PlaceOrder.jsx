1cff2412d08f3685c0d23643fc0f8626b6a60c15
shri3301 <shri21294@gmail.com>
2026-06-03 14:19:42 +0530

    Fix case-sensitivity mismatch in StoreContext import for PlaceOrder

--- QuickBite-main/Frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ QuickBite-main/Frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { StoreContext } from "../../Context/StoreContext";
+import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
